f9879e461ca5a0aab79eaa7cd5ef554db11cd2e2
Nedelin-Dmitry <90863146+Nedelin-Dmitry@users.noreply.github.com>
2023-03-27 19:24:20 +0300

    Add files via upload

---
+++ pcGraphicsLab3Sem/Image_Processing/Image_Processing/Filters.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.ComponentModel;
+
+namespace Image_Processing
+{
+    abstract class Filters
+    {
+        protected abstract Color calculateNewPixelColor(Bitmap sourceImage, int x, int y);
+
+        public int Clamp(int value, int min, int max)
+        {
+            if(value < min) { return min; }
+            if(value > max) { return max; }
+            return value;
+        }
+
+        
+
+
+        public Bitmap processImage(Bitmap sourseImage, BackgroundWorker worker)
+        {
+            Bitmap resultImage = new Bitmap(sourseImage.Width, sourseImage.Height);
+            for(int i = 0; i < sourseImage.Width; i++)
+            {
+                worker.ReportProgress((int)(float)i / resultImage.Width * 100);
+                if (worker.CancellationPending) { return null; }
+                for(int j = 0; j < sourseImage.Height; j++)
+                {
+                    resultImage.SetPixel(i, j, calculateNewPixelColor(sourseImage, i, j));
+                }
+            }
+
+            return resultImage;
+        }
+
+    }
+
+}
